bfd18bbef3b4b0551b0d42761e7a1a8513402a37
levi_wasa <leviwasa@gmail.com>
2025-11-02 03:39:34 +0200

    first commit

--- js/script.js
+++ js/script.js
@@ -3,12 +3,27 @@ const taskFieldEl = document.querySelector('.task-input input');
 const btnAddEl = document.querySelector('.add-task');
 const taskListEl = document.querySelector('.list');
 
+const testingCheck = document.querySelectorAll('.list .task');
+
 // Function section
 const addTask = () =>{
-    if(taskFieldEl.value == '') return
-
-    const taskDiv = document
+    // return if theres no text in input field 
+    if(taskFieldEl.value === '') return
+    try{
+       taskListEl.innerHTML += `
+            <li class="task">
+                <input type="checkbox" name="task">
+                <div>${taskFieldEl.value}</div>
+            </li>
+       `
+    }catch(err){
+        console.log(`something went wrong: ${err}`)
+    }
+    // clearing input field 
+    taskFieldEl.value = '';
 };
+
+// check if task as been checked or not 
 
 // Running 
 btnAddEl.addEventListener('click',addTask);
