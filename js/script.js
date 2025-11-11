c17dcf94d224561699eb943c96df5079a11fae1a
levi_wasa <leviwasa@gmail.com>
2025-11-11 02:34:20 +0200

    added filters btn

--- js/script.js
+++ js/script.js
@@ -1,24 +1,39 @@
 // Feature needed -
 // - loacl storage understanding 
-// - filtering tasks : All, Pendding, Complete
 // ==============================================================
 
 // Selector section 
 const formEl = document.querySelector('.form'); // add button
 const taskInputEl = document.querySelector('.form input'); // input field
 const taskListEl = document.querySelector('.tasks-box'); // task container
+const filters = document.querySelectorAll(".filters span");// filters buttons
 
 // local storage section
 // setting name storage and parsing it
-let todos = JSON.parse(localStorage.getItem('todos')); 
+let todos = JSON.parse(localStorage.getItem('todos'));
+
+// filters buttons 
+filters.forEach(btn=>{
+    // filters button added event listener
+    btn.addEventListener('click', ()=>{
+        // remove class 'active' from the first span that has it 
+        document.querySelector('span.active').classList.remove('active');
+        // add class 'active' on the clicked btn
+        btn.classList.add('active');
+        // add the id value attribute has arg to showTodos function
+        showTodos(btn.id);
+    })
+});
 
 // Function section
 const showTodos = (filter)=>{
     let liEl = '';
     // if todos exists
     if(todos){
+        // for each data object create task template
         todos.forEach((todo,id)=>{
-            let completed = todos.status == "Completed"?"check":"";
+            // check data object status 
+            let completed = todo.status == "completed"?"checked":"";
             if(filter == todo.status || filter == "all"){
                 liEl += `
                     <li class="task">
@@ -26,27 +41,43 @@ const showTodos = (filter)=>{
                             <input onClick="updateStatus(this)" id="${id}" type="checkbox" ${completed}/>
                             <p class="${completed}">${todo.name}</p>
                         </label>
+                        <div class="setting">
+                            <span onclick="deleteTask(${id},'${filter}')">❌</span>
+                        </div>
                     </li>
                 `;
             }
         });
-
     }
     taskListEl.innerHTML = liEl || `<span>there's no task here</span>`;
+    taskListEl.offsetHeight >= 300? taskListEl.classList.add('overflow'): taskListEl.classList.remove('overflow');
     // let checkTask = taskListEl.querySelectorAll(".task")
 };
 
 // update task
+// task checkbox checking for events 
 const updateStatus = (e)=>{
+// task check box checking for events 
     let taskName = e.parentElement.lastElementChild;
     if(e.checked){
-        taskName.classList.add('check');
-        todos[e.id].status = "Completed";
+        taskName.classList.add('checked');
+        todos[e.id].status = "completed";
     }else{
-        taskName.classList.remove('check');
-        todos[e.id].status = "Pending";
+        taskName.classList.remove('checked');
+        todos[e.id].status = "pending";
     }
     localStorage.setItem('todos', JSON.stringify(todos));
+};
+
+// delete task -  take two parrameters 
+// 1. task id, 2.filter for aditing the localStorage
+const  deleteTask = (taskId,filter) =>{
+    // splice - array method that take two parameters,
+    // one get the position of an element,
+    // the number of 'step' to cut from the item
+    todos.splice(taskId,1);
+    localStorage.setItem('todos', JSON.stringify(todos));
+    showTodos(filter);
 };
 
 const addTask = (e) =>{
@@ -59,13 +90,13 @@ const addTask = (e) =>{
     // if it does get them
     todos = !todos ? [] : todos;
     // create new task object
-    let newTaskData = {name:userTask,status:"Pending"};
+    let newTaskData = {name:userTask,status:"pending"};
     todos.push(newTaskData);
     // clear input field 
     taskInputEl.value = "";
     // stored the new data in an exsits or new todos storage
     localStorage.setItem("todos", JSON.stringify(todos));
-    // show current tasks list data
+    // show current tasks list data by 
     showTodos(document.querySelector("span.active").id);
 };
 
